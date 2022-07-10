9a9ed0516167abcd9f8722de535d66e8bc29b4c4
NABIN ACHAR <nabinacharofficial@gmail.com>
2022-07-10 15:46:24 +0530

    mapbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map, {
+  Marker,
+  NavigationControl,
+  Popup,
+  FullscreenControl,
+  GeolocateControl,
+} from "react-map-gl";
+import { useState } from "react";
 function App() {
+  const [lng, setLng] = useState(54.37585762735543);
+  const [lat, setLat] = useState(24.45677614934833);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {console.log(process.env.REACT_APP_MAP_KEY)}
+      <h1>Mapbox tutorial</h1>
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAP_KEY}
+        style={{
+          width: "500px",
+          height: "500px",
+          borderRadius: "15px",
+          border: "2px solid red",
+        }}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker longitude={lng} latitude={lat} />
+        <NavigationControl position="bottom-right" />
+        <FullscreenControl />
+
+        <GeolocateControl />
+      </Map>
     </div>
   );
 }
